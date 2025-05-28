1f7c091796fbeb0e7a84d6496becfe99b7ff09a7
MdicaL7 <507361324@qq.com>
2025-05-29 00:31:21 +0800

    修改参数

--- src/api/liveRoom.js
+++ src/api/liveRoom.js
@@ -2,10 +2,10 @@ import axios from "./axios";
 
 //获取单个直播间详情
 export function getLiveRoomDetail(id){
-    return axios.get(`/v1/api/liveRoom/${id}`)
+    return axios.get(`/liveRoom/${id}`)
 }
 
 //获取直播间列表
 export function getLiveRoomList() {
-  return axios.get('/v1/api/liveRoom')
+  return axios.get('/liveRoom')
 }
